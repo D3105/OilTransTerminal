6138c8207c03cf07a532dffe75cb7be1c4333bf6
d <rlh1191@gmail.com>
2018-05-31 19:38:34 +0300

    added Photoview library for image zooming and fixed warnings

--- app/src/main/java/com/d/oiltransterminal/MapActivity.kt
+++ app/src/main/java/com/d/oiltransterminal/MapActivity.kt
@@ -2,11 +2,13 @@ package com.d.oiltransterminal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_map.*
 
 class MapActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        imageView.setImageResource(R.drawable.map)
     }
 }
